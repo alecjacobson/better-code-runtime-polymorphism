72419f09fb5098be3cb719cd1def069e81d35dc4
Alec Jacobson <alecjacobson@gmail.com>
2018-08-29 16:29:24 -0400

    slide 42

--- library.hpp
+++ library.hpp
@@ -9,13 +9,25 @@ void draw(const int& x, ostream& out, size_t position)
 
 class object_t {
   public:
-    object_t(const int& x) : self_(x)
+    object_t(const int& x) : self_(make_unique<int_model_t>(x))
     { }
+    object_t(const object_t& x) : self_(make_unique<int_model_t>(*x.self_))
+    { }
+    object_t& operator=(const object_t& x)
+    { object_t tmp(x); self_ = move(tmp.self_); return *this; }
 
     friend void draw(const object_t& x, ostream& out, size_t position)
-    { draw(x.self_, out, position); }
+    { x.self_->draw_(out, position); }
   private:
-    int self_;
+    struct int_model_t {
+      int_model_t(const int& x) : data_(x) { }
+      void draw_(ostream& out, size_t position) const
+      { draw(data_, out, position); }
+
+      int data_;
+    };
+
+    unique_ptr<int_model_t> self_;
 };
 
 using document_t = vector<object_t>;
